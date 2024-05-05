8093b87aa8261a7394c10488948c161dea0bb773
Nayan Asawa <91011965+nayanasawa@users.noreply.github.com>
2024-05-05 12:00:16 +0530

    input

---
+++ form_inputs.py
@@ -0,0 +1,24 @@
+# form_inputs.py
+
+from selenium.webdriver.support.ui import Select
+
+# Define your form input variables here
+date = "24/04/2024"
+return_date = "25/04/2024"
+origin = "HYD"
+destination = "BLR"
+trip_type = "Round Trip"
+adults = 2
+childs = 1
+infants = 0
+email = "nayan.asawa@nineleaps.com"
+first1_N = "Nayan"
+last1_N = "Asawa"
+first2_N = "Krishna"
+last2_N = "Vyas"
+fchild1 = "Madhav"
+lchild1 = "Asawa"
+phone = "7793992929"
+card1 = "371449635398431"
+c1name = "NAYAN ASAWA"
+cvv = "199"
